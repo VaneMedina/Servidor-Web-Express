7dfa87704c3035e97e98eb213e22bcf2668ed7ac
Vanesa <mv382038@gmail.com>
2022-01-29 14:23:51 -0300

    Servidor que muestra los todos los productos y uno random

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const http = require('http')
+const express = require('express')
+const Contenedor = require('./main');
 
-const server = http.createServer((peticion, respuesta) => {
-   respuesta.end('Chau mundo cruel!')
+const instancia = new Contenedor("./products.json");
+
+
+//(app) Aplicación servidor 
+const app = express()
+
+app.get('/productos', async (req, res) => {
+   const products = await instancia.getAll();
+   res.send(JSON.stringify(products));
 })
 
-const connectedServer = server.listen(8080, () => {
-   console.log(`Servidor Http escuchando en el puerto ${connectedServer.address().port}`)
+app.get('/productoRandom', async (req, res) => {
+   const products = await instancia.getAll();
+   const product = products[Math.floor(Math.random() * products.length)];
+   res.send(JSON.stringify(product));
 })
+
+const PORT =  process.env.PORT || 8080
+const server = app.listen(PORT, () => {
+   console.log(`Servidor https escuchando en el puerto ${PORT}`);
+});
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
---
+++ main.js
@@ -0,0 +1,65 @@
+const fs = require('fs').promises;
+
+class Contenedor{
+    constructor(nameFile){
+        this.nameFile = nameFile;
+        this.id = 1
+    }
+
+    async save(product) {
+        this.id++
+        //Leo y almaceno los datos del archivo en una constante.
+        product.id = this.id
+        try{
+            const dataFile = await fs.readFile(this.nameFile);
+            const data = JSON.parse(dataFile);
+            data.push(product);
+            await fs.writeFile(this.nameFile, JSON.stringify(data, null, 2), 'utf-8');
+            return product.id;
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    async getById(id){
+        try {
+            const dataFile = await fs.readFile(this.nameFile);
+            const products = JSON.parse(dataFile);
+            const product = products.find(product => product.id === id);
+            if(product){
+                return product;
+            }else{
+                return null;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async getAll(){
+        const dataFile = await fs.readFile(this.nameFile);
+        const products = JSON.parse(dataFile);
+        return products;
+    }
+    async deleteById(id){
+        try {
+            const dataFile = await fs.readFile(this.nameFile);
+            const products = JSON.parse(dataFile);
+            //Devuelve un array con todos los elementos que tenga, menos el que coincida con el id que le envío.
+            const filteredProducts = products.filter(product => product.id !== id);
+            await fs.writeFile(this.nameFile, JSON.stringify(filteredProducts, null, 2), 'utf-8');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async deleteAll(){
+        try {
+            await fs.writeFile(this.nameFile, '[]', 'utf-8');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+
+module.exports = Contenedor
